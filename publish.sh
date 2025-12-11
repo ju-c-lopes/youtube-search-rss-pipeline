e5d2df02e658e420af6c7cd60fd60eaba545ea95
Juliano <juliano.co.lopes@gmail.com>
2025-12-11 12:30:53 -0300

    comidas regionais inserted

--- publish.sh
+++ publish.sh
@@ -4,7 +4,6 @@ exec > /dev/null 2>&1
 
 # Captura o commit message vindo do Shortcuts
 entered_search="$1"
-echo "$entered_search"
 
 cd "$HOME/Documents/youtube-search-rss" || exit 1
 
@@ -14,6 +13,6 @@ export GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa -o IdentitiesOnly=yes"
 git add .
 
 # Commit seguro (evita erro se não houver mudanças)
-git commit -m "$entered_search inserted"
+git diff --cached --quiet || git commit -m "$entered_search inserted"
 
 git push origin main
